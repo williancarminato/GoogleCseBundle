4cc221fab18c8959d72e946354a69a68d0635461
Willian Carminato <williancarminato@gmail.com>
2014-02-27 15:39:25 -0300

    Changes in json parser results to return an array of CseApiResultItem

---
+++ Model/CseApiResultItem.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Carminato\GoogleCseBundle\Model;
+
+class CseApiResultItem
+{
+    private $kind;
+
+    private $title;
+
+    private $htmlTitle;
+
+    private $link;
+
+    private $displayLink;
+
+    private $snippet;
+
+    private $htmlSnippet;
+
+    private $cacheId;
+
+    private $formattedUrl;
+
+    private $pagemap;
+
+    /**
+     * @param string $cacheId
+     */
+    public function setCacheId($cacheId)
+    {
+        $this->cacheId = $cacheId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheId()
+    {
+        return $this->cacheId;
+    }
+
+    /**
+     * @param string $displayLink
+     */
+    public function setDisplayLink($displayLink)
+    {
+        $this->displayLink = $displayLink;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDisplayLink()
+    {
+        return $this->displayLink;
+    }
+
+    /**
+     * @param string $formattedUrl
+     */
+    public function setFormattedUrl($formattedUrl)
+    {
+        $this->formattedUrl = $formattedUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormattedUrl()
+    {
+        return $this->formattedUrl;
+    }
+
+    /**
+     * @param string $htmlSnippet
+     */
+    public function setHtmlSnippet($htmlSnippet)
+    {
+        $this->htmlSnippet = $htmlSnippet;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHtmlSnippet()
+    {
+        return $this->htmlSnippet;
+    }
+
+    /**
+     * @param string $htmlTitle
+     */
+    public function setHtmlTitle($htmlTitle)
+    {
+        $this->htmlTitle = $htmlTitle;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHtmlTitle()
+    {
+        return $this->htmlTitle;
+    }
+
+    /**
+     * @param string $kind
+     */
+    public function setKind($kind)
+    {
+        $this->kind = $kind;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKind()
+    {
+        return $this->kind;
+    }
+
+    /**
+     * @param string $link
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * @param string $pagemap
+     */
+    public function setPagemap($pagemap)
+    {
+        $this->pagemap = $pagemap;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPagemap()
+    {
+        return $this->pagemap;
+    }
+
+    /**
+     * @param string $snippet
+     */
+    public function setSnippet($snippet)
+    {
+        $this->snippet = $snippet;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSnippet()
+    {
+        return $this->snippet;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+
+} 
--- Service/Parser/JsonCseResponseParser.php
+++ Service/Parser/JsonCseResponseParser.php
@@ -2,20 +2,48 @@
 
 namespace Carminato\GoogleCseBundle\Service\Parser;
 
+use Carminato\GoogleCseBundle\Model\CseApiResultItem;
 
 class JsonCseResponseParser implements CseResponseParserInterface
 {
+    /**
+     * @param $content
+     *
+     * @return array|null
+     */
     public function parseResults($content)
     {
         $decodedContent = json_decode($content, true);
 
         $items = null;
         if (isset($decodedContent['items'])) {
-            foreach ($decodedContent['items'] as $item) {
+            foreach ($decodedContent['items'] as $arrayItemProperties) {
+                $item = $this->generateCseResultApiItem($arrayItemProperties);
+
                 $items[] = $item;
             }
         }
 
         return $items;
+    }
+
+    /**
+     * @param array $itemProperties
+     *
+     * @return CseApiResultItem
+     */
+    private function generateCseResultApiItem(array $itemProperties)
+    {
+        $item = new CseApiResultItem();
+
+        foreach ($itemProperties as $property => $value) {
+            $setPropertyMethod = 'set'. ucfirst($property);
+
+            if (method_exists($item, $setPropertyMethod)) {
+                $item->$setPropertyMethod($value);
+            }
+        }
+
+        return $item;
     }
 }
